d492ab4fd8c9153d0b81234e94ecb4ddfb3d48f4
josents <jose.neto@waproject.com.br>
2021-02-21 10:55:58 -0300

    Definindo modulos aninhados

--- multiplicador.ex
+++ multiplicador.ex
@@ -3,3 +3,17 @@ defmodule Multiplicador do
   def multiplique(a, b \\ 2), do: a * b
   defp soma(a, b), do: a + b
 end
+
+#Módulo aninhado
+defmodule Multiplicador do
+  defmodule Multiplique do
+    def por_dois(numero) do
+      numero * 2
+    end
+  end
+end
+
+#Módulo aninhado com ponto
+defmodule Multiplicador.Multiplique do
+  def por_dois(numero), do: numero * 2
+end

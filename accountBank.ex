f1bc50190fb56ebb99d798a8271cc90127f9ba22
josents <jose.neto@waproject.com.br>
2021-02-21 15:17:23 -0300

    Add credit sum available

--- accountBank.ex
+++ accountBank.ex
@@ -9,5 +9,13 @@ defmodule AccountBank do
   end
   defmodule AccountBalance do
     def getTotalBalance(correnteBalance, poupancaBalance \\ 0), do: correnteBalance + poupancaBalance
+    #Param list of credits for sum
+    #Passar a lista de creditos para a soma
+    def getCreditAvailable(balances) do
+      if is_list balances do
+        Enum.filter(balances, &(&1 > 0))|>
+        Enum.sum()
+      end
+    end
   end
 end
